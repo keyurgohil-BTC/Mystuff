ab843476820e2904beaf9c6f271fd809b62a8c32
Keyur Gohil <keyurgohil22@gmail.com>
2017-01-16 13:31:30 +0530

    Beer_song_v2

--- beer_song.rb
+++ beer_song.rb
@@ -1,33 +1,28 @@
 class BeerSong
+
+  def get_s(beer_bottles)
+    case beer_bottles
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{beer_bottles} bottles of beer on the wall, #{beer_bottles} bottles of beer.\nTake one down and pass it around, #{beer_bottles - 1} bottles of beer on the wall.\n"
+    end
+  end
+
   def verse(beer_bottles)
-    expected = case beer_bottles
-                when 0
-                  "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-                when 1
-                  "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-                when 2
-                  "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-                else
-                  "#{beer_bottles} bottles of beer on the wall, #{beer_bottles} bottles of beer.\nTake one down and pass it around, #{beer_bottles - 1} bottles of beer on the wall.\n"
-                end
+    get_s(beer_bottles)
   end
 
   def verses(start,toend)
-    expected = ""
-    (start).downto(toend).each do |beer_bottles_e|
-      expected += verse(beer_bottles_e)
-      expected += if_more(beer_bottles_e,toend)
-    end
-    expected
+    start.downto(toend).map { |beer_bottles_e| verse(beer_bottles_e) }.join("\n")
   end
 
   def lyrics
-    expected = ""
-    (99).downto(0).each do |beer_bottles_e|
-      expected += verse(beer_bottles_e)
-      expected += if_more(beer_bottles_e,0)
-    end
-    expected
+    verses(99,0)
   end
 
   def if_more(beer_bottles_e,last)
