16bd356c78c56cece7ed450b431a478296c8cee8
Keyur Gohil <keyurgohil22@gmail.com>
2017-01-17 12:56:31 +0530

    Nucleotide-count-v3

--- nucleotide_count.rb
+++ nucleotide_count.rb
@@ -3,20 +3,15 @@ class Nucleotide
     raise ArgumentError unless is_s(dna_stream)
     Nucleotide.new(dna_stream)
   end
-
   def initialize(dna_stream)
     @dna_stream = dna_stream.upcase
   end
 
   def count(nucleotide)
-    return 0 if @dna_stream.nil?
-    @dna_stream.split("").inject(0) { |c, chr| c += (( chr == nucleotide )? 1 : 0)}
+    @dna_stream.count(nucleotide)
   end
-
   def histogram
-    asides = { "A" => 0, "C" => 0, "G" => 0,"T" => 0}
-    asides.keys.map { |key| asides[key] = count(key) }
-    asides
+    %w[A C G T].each_with_object({}) { |key,asides| asides[key] = count(key) }
   end
 
   def self.is_s(string_val)
