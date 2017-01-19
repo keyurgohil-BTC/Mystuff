c54a69ad346a088e2611e9b83b4cb706b960381e
Keyur Gohil <keyurgohil22@gmail.com>
2017-01-19 17:14:38 +0530

    Sieve-v1

---
+++ sieve.rb
@@ -0,0 +1,16 @@
+module BookKeeping
+  VERSION = 1
+end
+
+require 'prime'
+
+class Sieve
+	def initialize(number)
+		@number = number
+	end
+	def primes
+		return [] if @number <= 1
+		a = []				
+		Prime.each(@number) { |e| a.push(e) }
+	end
+end
